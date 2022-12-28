19cc8abba5ee07cddf32ee05af57b207ac88e8d2
Kevin Kodama <kevinkodama@Kevins-MacBook-Pro.local>
2022-12-28 13:46:46 -0800

    Attempt to add mongoose stuff, probably will fail to deploy

---
+++ methods/addCharts.js
@@ -0,0 +1,80 @@
+/*
+ * u/bright_baby_blue
+ * 02/28/2022
+ *
+ * This method adds new charts to the database.
+ */
+"use strict";
+
+const ChartModel = require('../models/Chart');
+const { parseHTML } = require("../methods/parseHTML");
+const { parsePastes } = require("../methods/parsePastes");
+const { processComments } = require("../methods/processComments");
+const { wikiPageToHTML } = require("../methods/wikiPageToHTML");
+const { getDate } = require("../methods/getDate");
+
+async function addCharts(r, chartPosts, chartSize) {
+  for(let i = 0; i < chartPosts.length; i++) {
+    let currIndex = i + chartSize;
+    let post = await r
+      .getSubmission(chartPosts[i])
+      .fetch();
+    let redditURL = await post.url;
+    let title = await post.title;
+    let titleInfo = "<h2><a href=" +
+      await post.url + ">" +
+      await post.title +
+      "</a></h2>";
+    let parsedDate = getDate(title);
+    let pastes = ["",""];
+    let postHTML = post.selftext_html;
+    pastes = parseHTML(postHTML);
+    let songData = "SONGS EMPTY";
+    if(i != 19) {
+      songData = await parsePastes(pastes, 0, titleInfo);
+    }
+    let fullCharts = await post
+      .expandReplies({limit: 100, depth: 2}).comments
+      .then(processComments)
+      .then(parseHTML)
+      .catch(err => {console.error(err); [];});
+    if((i != 19) && (pastes[1].includes("reddit"))) {
+      let albumData = await wikiPageToHTML(pastes[1], titleInfo, r);
+      const myChart = new ChartModel({
+        index: currIndex,
+        code: chartPosts[i],
+        title: title,
+        date: parsedDate,
+        redditURL: redditURL,
+        songData: songData,
+        albumData: albumData,
+        fullCharts: fullCharts
+      });
+      myChart.save();
+    } else {
+      let albumData = "ALBUMS EMPTY";
+      if(i != 19) {
+        albumData = await parsePastes(pastes, 1, titleInfo);
+      } else {
+        console.log(title);
+        console.log(parsedDate);
+        console.log(redditURL);
+        console.log(songData);
+        console.log(albumData);
+        console.log(fullCharts);
+      }
+      const myChart = new ChartModel({
+        index: currIndex,
+        code: chartPosts[i],
+        title: title,
+        date: parsedDate, 
+        redditURL: redditURL,
+        songData: songData,
+        albumData: albumData,
+        fullChartURL: fullCharts
+      });
+      await myChart.save();
+    }}
+    console.log("DONE ADDING CHARTS");
+}
+exports.addCharts = addCharts;
---
+++ methods/processComments.js
@@ -0,0 +1,48 @@
+/*
+ * u/bright_baby_blue
+ * 02/28/2022
+ *
+ * This is the server-side JavaScript for the popheads chart, which allows
+ * you to make API calls to the full chart.
+ */
+"use strict";
+/**
+ * Checks comment thread to find the comment with full chart
+ * @param {Array} threadComments array of all top level comments in thread
+ * @returns {String} comment containing full chart
+ */
+async function processComments(threadComments) {
+  console.log("running");
+  for (let i = 0; i < threadComments.length; i++) {
+    const comm = threadComments[i];
+    if(testComment(comm)) {
+      console.log(comm.author.name);
+      return threadComments[i].body_html;
+    } else {
+      for (let j = 0; j < comm.replies.length; j++) {
+        if (testComment(comm)) {
+          return comm.replies[j].body_html;
+        }
+      }
+    }
+  }
+}
+
+/**
+ * Checks comment to see if it is a Full Chart candidate
+ * @param {Object} comm current comment
+ * @returns {Boolean} true if comment is Full Chart candidate, otherwise false
+ */
+function testComment(comm) {
+  if((comm.is_submitter ||
+    comm.author.name == 'letsallpoo' || 
+    comm.author.name == 'ImADudeDuh') && 
+   (comm.body_html.includes('pastebin') ||
+    comm.body_html.includes('paste.ee'))
+  ) {
+    return true;
+  }
+  return false;
+}
+
+exports.processComments = processComments;
