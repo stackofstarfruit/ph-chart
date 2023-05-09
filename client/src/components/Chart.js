a1c47bd2b1ec93a83fa6f270c8518a411b3a6a96
Kevin Kodama <kevinkodama@Kevins-MacBook-Pro.local>
2023-05-09 10:19:42 -0700

    Set default value to most recent

--- client/src/components/Chart.js
+++ client/src/components/Chart.js
@@ -1,33 +1,50 @@
 import { useEffect, useState } from "react";
-import '../App.css';
-import WeekMenu from './WeekMenu'
-import UpdateButton from './UpdateButton'
+import "../App.css";
+import WeekMenu from "./WeekMenu";
+import UpdateButton from "./UpdateButton";
 
 function Chart() {
   const [currChart, setCurrChart] = useState(0);
   const [chartType, setChartType] = useState(0);
-  const [currWeek, setCurrWeek] = useState(289);
+  const [currWeek, setCurrWeek] = useState(null);
   const [currChartHTML, setCurrChartHTML] = useState();
+  const [weekData, setWeekData] = useState(null);
 
   useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("/weeks");
+      const data = await res.json();
+      const reversedData = data.reverse();
+      setWeekData(reversedData);
+      setCurrWeek(reversedData[0].value);
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (currWeek === null) return;
+
     const abortController = new AbortController();
-    
+
     const fetchData = async () => {
       setTimeout(async () => {
         try {
-          const weekData = await fetch(`/chart?chartNum=${currWeek}&chartType=${chartType}`,
-            { signal: abortController.signal });
+          const weekData = await fetch(
+            `/chart?chartNum=${currWeek}&chartType=${chartType}`,
+            { signal: abortController.signal }
+          );
           const chartHTML = await weekData.text();
           setCurrChartHTML(chartHTML);
-          } catch (error) {
-            if(error.name === 'AbortError') {
-              // no error
-            } else {
-              console.error();
-            }
+        } catch (error) {
+          if (error.name === "AbortError") {
+            // no error
+          } else {
+            console.error();
           }
-      }, Math.round(Math.random() * 100))
-    }
+        }
+      }, Math.round(Math.random() * 100));
+    };
 
     fetchData();
 
@@ -40,8 +57,8 @@ function Chart() {
     event.preventDefault();
     let fetchURL = "/chart?chartNum=" + currChart + "&chartType=" + chartType;
     fetch(fetchURL)
-      .then(res => res.text())
-      .then(res => setCurrChart(res))
+      .then((res) => res.text())
+      .then((res) => setCurrChart(res))
       .catch(console.error);
   }
 
@@ -54,35 +71,39 @@ function Chart() {
       <h1 className="section-title">Popheads Chart</h1>
       <section>
         <form onSubmit={getChart}>
-          <WeekMenu currWeek = {currWeek} setCurrWeek = {setCurrWeek}/>
-            <br />
-            <input 
-              type="radio"
-              value="0"
-              name="chartType"
-              defaultChecked
-              onChange={handleChange}
-            />Get Songs
-            <input
-              type="radio"
-              value="1"
-              name="chartType"
-              onChange={handleChange}
-            />Get Albums (deprecated, displays users signed up for the charts after 01/02/2023)
-            <br />
-            <input 
-              type="radio"
-              value="2"
-              name="chartType"
-              onChange={handleChange}
-            />Get ALL Songs (only works after 12/14/2020)
+          {weekData && currWeek ? (
+            <WeekMenu currWeek={currWeek} setCurrWeek={setCurrWeek} weekData={weekData} />
+          ) : (
+            <p>Loading...</p>
+          )}
+          <br />
+          <input
+            type="radio"
+            value="0"
+            name="chartType"
+            defaultChecked
+            onChange={handleChange}
+          />Get Songs
+          <input
+            type="radio"
+            value="1"
+            name="chartType"
+            onChange={handleChange}
+          />Get Albums (deprecated, displays users signed up for the charts after 01/02/2023)
+          <br />
+          <input
+            type="radio"
+            value="2"
+            name="chartType"
+            onChange={handleChange}
+          />Get ALL Songs (only works after 12/14/2020)
           <br />
           <UpdateButton />
         </form>
-        <div dangerouslySetInnerHTML={{__html:currChartHTML}}></div>
+        <div dangerouslySetInnerHTML={{ __html: currChartHTML }}></div>
       </section>
     </section>
-  )
+  );
 }
 
 export default Chart;
--- client/src/components/WeekMenu.js
+++ client/src/components/WeekMenu.js
@@ -1,29 +1,19 @@
 import React from "react";
 import Select from "react-select";
-import { useEffect, useState } from "react";
 
-function WeekMenu({currWeek, setCurrWeek}) {
-
-  const [weekData, setWeekData] = useState([{ label: 'Mon Dec 26 2022', value: 287 }]);
-  useEffect(() => {
-    fetch("/weeks")
-      .then(res => res.json())
-      .then(res => res.reverse())
-      .then(res => {setWeekData(res)})
-  }, [currWeek]);
-
-  function handleChange(event) { // whole object of selected option 
+function WeekMenu({ currWeek, setCurrWeek, weekData }) {
+  function handleChange(event) {
     setCurrWeek(event.value);
-  };
+  }
 
   return (
-    <section class="select-region">
+    <section className="select-region">
       <Select
         options={weekData}
-        value={weekData.find(obj => obj.value === currWeek)}
+        value={weekData.find((obj) => obj.value === currWeek)}
         onChange={handleChange}
       />
     </section>
-  )
+  );
 }
 export default WeekMenu;
