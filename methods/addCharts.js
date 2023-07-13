fe1ea91935349ff65a2387e84fb3e9ab95caf27f
Kevin Kodama <kevinkodama@Kevins-MacBook-Pro.local>
2023-07-13 16:08:37 -0700

    Fix duplicate database entry error handling

--- methods/addCharts.js
+++ methods/addCharts.js
@@ -1,9 +1,3 @@
-/*
- * u/bright_baby_blue
- * 02/28/2022
- *
- * This method adds new charts to the database.
- */
 "use strict";
 
 const ChartModel = require('../models/Chart');
@@ -12,64 +6,71 @@ const { parsePastes } = require("../methods/parsePastes");
 const { processComments } = require("../methods/processComments");
 const { wikiPageToHTML } = require("../methods/wikiPageToHTML");
 const { getDate } = require("../methods/getDate");
-const { storeArtistWeek } = require('./storeArtistWeek');
 const { storeFullChart } = require('./storeFullChart');
 const { RateLimitError } = require('snoowrap');
 
 async function addCharts(r, chartPosts, chartSize) {
   try {
     const operations = chartPosts.map(async (postCode, i) => {
-      const currIndex = chartSize + i;
-      const post = await fetchWithRetry(() => r.getSubmission(postCode).fetch());
-      const redditURL = post.url;
-      const title = post.title;
-      const titleInfo = `<h2><a href="${post.url}">${post.title}</a></h2>`;
-      const parsedDate = getDate(title);
-      let pastes = ["", ""];
-      const postHTML = post.selftext_html;
+      const exists = await ChartModel.findOne({ code: postCode });
 
-      pastes = parseHTML(postHTML);
+      if (!exists) {
+        const currIndex = chartSize + i;
+        const post = await fetchWithRetry(() => r.getSubmission(postCode).fetch());
+        const redditURL = post.url;
+        const title = post.title;
+        const titleInfo = `<h2><a href="${post.url}">${post.title}</a></h2>`;
+        const parsedDate = getDate(title);
+        let pastes = ["", ""];
+        const postHTML = post.selftext_html;
 
-      let songData = "SONGS EMPTY";
-      if (i !== 19) {
-        songData = await parsePastes(pastes, 0, titleInfo);
-      }
+        pastes = parseHTML(postHTML);
 
-      const fullCharts = await fetchWithRetry(() =>
-        post.expandReplies({ limit: 100, depth: 2 })
-          .comments.then(processComments)
-          .then(parseHTML)
-      );
+        let songData = "SONGS EMPTY";
+        if (i !== 19) {
+          songData = await parsePastes(pastes, 0, titleInfo);
+        }
 
-      let albumData = "ALBUMS EMPTY";
-      if (i !== 19 && pastes[1].includes("reddit")) {
-        albumData = await wikiPageToHTML(pastes[1], titleInfo, r);
-      } else if (i !== 19) {
-        albumData = await parsePastes(pastes, 1, titleInfo);
+        const fullCharts = await fetchWithRetry(() =>
+          post.expandReplies({ limit: 100, depth: 2 })
+            .comments.then(processComments)
+            .then(parseHTML)
+        );
+
+        let albumData = "ALBUMS EMPTY";
+        if (i !== 19 && pastes[1].includes("reddit")) {
+          albumData = await wikiPageToHTML(pastes[1], titleInfo, r);
+        } else if (i !== 19) {
+          albumData = await parsePastes(pastes, 1, titleInfo);
+        } else {
+          console.log(title);
+          console.log(parsedDate);
+          console.log(redditURL);
+          console.log(songData);
+          console.log(albumData);
+          console.log(fullCharts);
+        }
+
+        return {
+          index: currIndex,
+          code: postCode,
+          title,
+          date: parsedDate,
+          redditURL,
+          songData,
+          albumData,
+          fullChartURL: fullCharts,
+        };
       } else {
-        console.log(title);
-        console.log(parsedDate);
-        console.log(redditURL);
-        console.log(songData);
-        console.log(albumData);
-        console.log(fullCharts);
+        console.log(`Post ${postCode} already exists in the database.`);
+        return null;
       }
-
-      return {
-        index: currIndex,
-        code: postCode,
-        title,
-        date: parsedDate,
-        redditURL,
-        songData,
-        albumData,
-        fullChartURL: fullCharts,
-      };
     });
 
     const results = await Promise.all(operations);
+    const validResults = results.filter(result => result != null);
 
-    results.forEach(async (chartData) => {
+    validResults.forEach(async (chartData) => {
       const myChart = new ChartModel(chartData);
       await myChart.save();
       setTimeout(storeFullChart, 5000, chartData.index);
@@ -83,7 +84,7 @@ async function addCharts(r, chartPosts, chartSize) {
 
 async function fetchWithRetry(fetchFn) {
   const maxRetries = 3;
-  let retryDelay = 1000; // Initial delay in milliseconds
+  let retryDelay = 1000; 
 
   for (let i = 0; i < maxRetries; i++) {
     try {
@@ -92,7 +93,7 @@ async function fetchWithRetry(fetchFn) {
       if (error.name === 'RateLimitError') {
         console.log('Rate limit error occurred. Retrying in ' + retryDelay + 'ms...');
         await new Promise((resolve) => setTimeout(resolve, retryDelay));
-        retryDelay *= 2; // Double the delay for the next retry
+        retryDelay *= 2; 
       } else {
         throw error;
       }
@@ -100,16 +101,6 @@ async function fetchWithRetry(fetchFn) {
   }
 
   throw new Error('Exceeded maximum retry attempts');
-}
-
-function isRateLimitError(error) {
-  // Check if the error message contains rate limit related information
-  const errorMessage = error.message || '';
-  return errorMessage.includes('rate limit') || errorMessage.includes('ratelimit');
-}
-
-function wait(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 exports.addCharts = addCharts;
